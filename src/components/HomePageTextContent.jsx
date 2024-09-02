ef66e88fb5537a7d8e7cfe243d66359a99652a95
Rob <rverplanke@gmail.com>
2024-09-02 16:45:33 +0200

    refactor: render text content with component

---
+++ src/components/HomePageTextContent.jsx
@@ -0,0 +1,21 @@
+export function HomePageTextContent() {
+  return (
+    <>
+      <h2>An aspiring Front-End Developer</h2>
+      <p>
+        <i>
+          With a passion for learning and building intuitive web experiences.
+        </i>
+        <br />
+        <br />
+        This portfolio showcases my journey so far, featuring projects that
+        highlight my growing skills in HTML,
+        <br /> CSS, JavaScript, and React. This portfolio showcases my journey
+        so far,
+        <br />
+        <br /> featuring projects that highlight my growing skills in HTML, CSS,
+        JavaScript, and React.
+      </p>
+    </>
+  );
+}
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,24 +1,11 @@
+import { HomePageTextContent } from '../HomePageTextContent.jsx';
 import { ProjectSelection } from '../ProjectSelection.jsx';
 
 function Home() {
   return (
     <main>
       {/* Display title and introduction text */}
-      <h2>An aspiring Front-End Developer</h2>
-      <p>
-        <i>
-          With a passion for learning and building intuitive web experiences.
-        </i>
-        <br />
-        <br />
-        This portfolio showcases my journey so far, featuring projects that
-        highlight my growing skills in HTML,
-        <br /> CSS, JavaScript, and React. This portfolio showcases my journey
-        so far,
-        <br />
-        <br /> featuring projects that highlight my growing skills in HTML, CSS,
-        JavaScript, and React.
-      </p>
+      <HomePageTextContent />
 
       {/* Display 'Project selection' content */}
       <ProjectSelection />
