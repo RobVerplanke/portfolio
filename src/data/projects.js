20eca6d47a106764db7caa8de54185d9e283f84d
Rob <rverplanke@gmail.com>
2024-09-02 15:35:07 +0200

    chore: update project content text

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,19 +3,9 @@ export const projects = [
     id: 1,
     title: 'Multi Shopee - Mock webshop',
     description:
-      'A mocked webshop with functional shopping cart and shopping page',
-    image: '../assets/shopping-cart-small.png',
-    liveLink: 'https://lovely-vacherin-d4496c.netlify.app/',
-    codeLink: 'https://github.com/RobVerplanke/project-shopping-cart',
-  },
-  {
-    id: 2,
-    title: 'Example 2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+      'This project demonstrates the use of React for building a mock shopping cart application. It includes features such as routing, state management, data fetching from an API and component testing with React Testing Library.',
     image: '../assets/shopping-cart-small.png',
     liveLink: 'https://lovely-vacherin-d4496c.netlify.app/',
     codeLink: 'https://github.com/RobVerplanke/project-shopping-cart',
   },
 ];
-// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteu
