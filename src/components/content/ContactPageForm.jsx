b2b90212487d8bea3db122c45d24de47644da06b
Rob <rverplanke@gmail.com>
2024-09-03 20:11:31 +0200

    fix: make input values readable and add more space
    Make input values readable by changing font color
    
    Add more height to Subject input field
    
    Add border radius to both input fields

--- src/components/content/ContactPageForm.jsx
+++ src/components/content/ContactPageForm.jsx
@@ -15,14 +15,18 @@ export default function ContactForm() {
           netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
+
           <label htmlFor="subject">Subject:</label>
           <br />
           <input type="text" id="subject" name="subject" />
           <br />
+
           <label htmlFor="content">Message:</label>
           <br />
           <textarea id="content" name="message"></textarea>
           <br />
+          <br />
+
           <button type="submit">Send</button>
         </form>
       </div>
