2dfa627e40d3b30b37a7f375f44067ec6d689aad
Rob <rverplanke@gmail.com>
2024-09-04 15:20:51 +0200

    fix: receive message after sent

--- src/components/content/ContactPageForm.jsx
+++ src/components/content/ContactPageForm.jsx
@@ -7,7 +7,12 @@ export default function ContactForm() {
       <div className="form-holder">
         <h3>Direct message</h3>
 
-        <form name="contact" action="/thank-you" data-netlify="true">
+        <form
+          name="contact"
+          action="/thank-you"
+          method="POST"
+          data-netlify="true"
+        >
           <input type="hidden" name="contact-form" value="contact" />
 
           <label htmlFor="subject">Subject:</label>
