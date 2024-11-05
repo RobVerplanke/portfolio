66d8ecb839a507d254a46df6dc1603791b7af012
Rob <rverplanke@gmail.com>
2024-11-05 18:04:44 +0100

    chore: update content text

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -5,43 +5,56 @@ function About() {
         <h2>Who am i</h2>
         <br />
         <p>
-          I am 42 years old and live in Groningen, where I completed an MBO in
-          Technical Informatics, followed by an HBO study in Communication &
-          Multimedia Design. During my internships at various web design
-          companies, I gained experience in developing and maintaining websites
-          in a professional setting.
+          Currently, I live in Groningen, where I completed an MBO in Technical
+          Informatics. I then pursued an HBO degree in Communication &
+          Multimedia Design. During internships at various web design companies,
+          I gained experience in developing and maintaining websites in a
+          professional setting
           <br />
           <br />
-          After my studies, I didn’t immediately find a position that aligned
-          with my ambitions, leading me to work in a different field. After
-          several years in call centers, I realized that these roles ultimately
-          offered me too little challenge. Following a period of reflection,
-          orientation and consideration, I reached a point where I understood it
-          was time to make a definitive career choice and invest in a field that
-          truly brings fulfillment: technology and programming.
+          In addition to working on web applications, I enjoy staying fit by
+          going to the gym regularly. I also like picking up the guitar and
+          making music from time to time. Just like with programming and web
+          development, there&apos;s always more to learn! Which keeps me
+          challenged. I&apos;ve noticed that these hobbies often give me a fresh
+          perspective on the web development projects I&apos;m working on and
+          help spark new ideas or better solutions.
           <br />
+          <br />
+          In my spare time, I also like to keep up with the latest developments
+          in the tech world. This helps me stay up-to-date with new knowledge
+          and gives me ideas for future projects
+          <br />
+          <br />
+          <h4>Motivation</h4>
+          After working in call centers for several years, I realized that these
+          roles ultimately didn&apos;t provide enough challenge. After a period
+          of exploration, I understood that I needed to invest in a field that
+          would truly give me fulfillment: technology and programming.
           <br />
           The possibilities within modern web development, such as working with
-          frameworks and new tools, fuled my passion. This discovery brought new
-          energy and reinforced my ambition to build a career in this area. I
-          then decided to deepen my knowledge through self-study in web
+          frameworks and new tools, refuled my passion. This discovery brought
+          new energy and strengthened my ambition to continue my career in this
+          field. I decided to deepen my knowledge through self-study in web
           development.
           <br />
           <br />
-          What appeals to me in web development is the combination of analytical
-          thinking and creating user-friendly solutions. Recognizing that
-          structured work is essential challenges me to design a well-founded
-          approach for each project and work with precision. When the final
-          result sometimes exceeds expectations, it brings me immense
-          satisfaction.
+          What excites to me about web development is the combination of
+          analytical thinking and creating user-friendly solutions. With each
+          project completed, I increasingly realize the importance of structured
+          work. This challenges me to develop a well-thought-out approach for
+          every task and to work with precision. My goal is always to exceed (my
+          own) expectations, which gives me great satisfaction and drives me to
+          keep challenging myself.
           <br />
           <br />
-          <h4>Future goals and development</h4>I have built a solid foundation
-          in HTML, CSS, JavaScript, TypeScript, and React and am continuously
-          working to enhance these and other skills. I am seeking opportunities
-          to contribute to web applications where I can apply my knowledge and
-          add value. For now, my focus is on front-end development, but I also
-          have the ambition to explore back-end development.
+          <h4>Future goals and development</h4>
+          I have built a solid foundation in HTML, CSS, JavaScript, TypeScript,
+          and React, and I actively work on developing these and other skills.
+          In my recent projects, I&apos;ve gained deeper insights into state
+          management, routing, and using reducers. For my latest project, I took
+          it a step further by diving into TypeScript and Tailwind CSS, which I
+          then applied directly.
           <br />
           <br />
           With my structured work ethic and passion for technology, I can
@@ -49,8 +62,8 @@ function About() {
           together to build high-quality applications.
           <br />
           <br />
-          Feel free to reach out if you’d like to connect or discuss potential
-          collaborations!
+          Feel free to reach out if you&apos;d like to connect or discuss
+          potential collaborations!
         </p>
         <br />
         <br />
